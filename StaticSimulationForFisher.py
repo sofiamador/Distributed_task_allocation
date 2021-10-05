671bcf2ee5e6ffa70aff8032a5554603e2cc38e8
benrachmut <rachmut.ben@gmail.com>
2021-10-05 13:53:54 +0300

    26th commit ben add static sim

--- StaticSimulationForFisher.py
+++ StaticSimulationForFisher.py
@@ -1,5 +1,6 @@
 import Allocation_Solver_Abstract
-from Simulation import MapHubs
+i
+from Simulation import MapHubs, TaskArrivalEvent,TaskGenerator
 from Allocation_Solver_Abstract import AllocationSolver
 simulation_reps = 100
 
@@ -10,23 +11,46 @@ simulation_reps = 100
 #players_per_scenario = create_tasks_per_scenario(tasks_per_scenario)
 #new_task_events_per_scenario = create_new_task_events_per_scenario(maps)
 
+class TaskArrivalEventStatic(TaskArrivalEvent):
+
+    def handle_event(self, simulation):
+        find_responsible_agent()
+        simulation.solve()
+
+
+class AllocationStaticGenerator:
+    """
+    create tasks and players and responsible players
+    """
+    pass
+
+
 class SimulationStatic():
-    def __init__(self, solver: AllocationSolver, rep_number, players_required_ratio,
+    def __init__(self, rep_number,solver: AllocationSolver, players_required_ratio,
                  number_of_centers = 3, tasks_per_center = 2,
                  length_y=9.0, width_x=9.0, sd_multiplier=0.5):
 
-        map = MapHubs(number_of_centers=number_of_centers, seed=rep_number,
+        self.seed_number = rep_number
+
+        self.map = MapHubs(number_of_centers=number_of_centers, seed=self.seed_number,
                                  length_y=length_y, width_x=width_x, sd_multiplier=sd_multiplier)
-
-        self.solver = solver
-
-        allocation_generator = Allocation_Static_Generator(seed = rep_number, map = map,tasks_per_center = tasks_per_center,
+        self.task_generator = TaskGenerator(self.map,self.seed_number) #get_next_task
+        #abilities,map,seed,max amount of missions, task class, mission task (need to extend)
+        allocation_generator = Allocation_Static_Generator(seed = self.seed_number, map = self.map,
+                                                           task_generator = self.task_generator,
+                                                           tasks_per_center = tasks_per_center,
                                                     players_required_ratio = players_required_ratio )
 
-        tasks = allocation_generator.tasks
-        players = allocation_generator.players
-        task_arrive_event
-        self.solver.add_tasks_list(tasks)
-        self.solver.add_players_list(players)
+        self.tasks = allocation_generator.tasks
+        self.players = allocation_generator.players
+
+        self.solver = solver
+        self.solver.add_tasks_list(self.tasks)
+        self.solver.add_players_list(self.players)
+        self.task_arrive_event =  self.create_task_arrival_event()
+
+    def create_task_arrival_event(self):
+        self.seed_number = self.seed_number*17
+
 
 
